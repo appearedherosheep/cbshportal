9660ecf1bcde69a5e09a3fd04e16e433a343ffc4
enc2586 <enc2586@gmail.com>
2022-03-04 14:22:26 +0900

    fix2

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-import express from 'express'
-import path from 'path'
+const path = require('path');
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '..', 'public');
